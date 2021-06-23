900f144d9e69066b3f71af2089e55c2aef26f04f
macbook-air <techcom.ec20@gmail.com>
2021-06-23 15:12:21 +0700

    Linq

---
+++ go.mod
@@ -0,0 +1,18 @@
+module gotest
+
+go 1.16
+
+require (
+	github.com/ahmetb/go-linq/v3 v3.2.0
+	github.com/c9s/gomon v1.3.0 // indirect
+	github.com/daviddengcn/go-colortext v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/ahmetb/go-linq/v3"
+)
+
+type Car struct {
+	id           int
+	year         int
+	owner, model string
+}
+
+func main() {
+
+	cars := []Car{}
+	cars = append(cars,
+		Car{
+			id:    111111,
+			year:  1,
+			owner: "11",
+		}, Car{
+			id:    222222,
+			year:  2,
+			owner: "22",
+		}, Car{
+			id:    333333,
+			year:  3,
+			owner: "33",
+		}, Car{
+			id:    444444,
+			year:  4,
+			owner: "44",
+		})
+
+	//## Linq
+	var result []Car
+	From(cars).WhereT(func(c Car) bool {
+		return c.id == 222222
+	}).SelectT(func(c Car) Car {
+		return c
+	}).ToSlice(&result)
+
+	fmt.Printf("%+v", result)
+	fmt.Println("======================\\n")
+	fmt.Println("======================")
+
+	//##Validate
+}
